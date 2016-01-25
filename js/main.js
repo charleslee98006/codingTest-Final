5f73dbee2aa80a7d89392d502c99670e34a359d3
charles <charles.98006@gmail.com>
2016-01-25 00:30:47 -0800

    DONE for IE

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ function transitVid(){
 	if (navigator.appVersion.indexOf("MSIE 10") !== -1){
     	$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"150px", "left":"100px"});
     	$("#sample-textbox").css({"top":"160px", "left":"950px"});
-    	$("#tos-button").css({"top":"510px", "left":"1200px"});
+    	$("#tos-button").css({"top":"610px", "left":"1250px"});
 	}
 	else{
 		$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"100px"});
