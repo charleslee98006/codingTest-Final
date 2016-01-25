ce0391a48e899847eb2a5153fa8e615b218f54c1
charles <charles.98006@gmail.com>
2016-01-25 00:24:01 -0800

    DONE for IE

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function transitVid(){
 	$( ".jp-video" ).fadeOut( "slow", function(){
 	if (navigator.appVersion.indexOf("MSIE 10") !== -1){
     	$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"200px", "left":"100px"});
-    	$("#sample-textbox").css({"top":"200px", "left":"600px"});
+    	$("#sample-textbox").css({"top":"210px", "left":"900px"});
 	}
 	else{
 		$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"100px"});
