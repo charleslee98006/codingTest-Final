9d7702e36b854b6fdc4ac0992f84c0cd18a9c4fb
charles <charles.98006@gmail.com>
2016-01-25 00:29:20 -0800

    DONE for IE

--- js/main.js
+++ js/main.js
@@ -52,9 +52,9 @@ function skipClick(){
 function transitVid(){
 	$( ".jp-video" ).fadeOut( "slow", function(){
 	if (navigator.appVersion.indexOf("MSIE 10") !== -1){
-    	$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"200px", "left":"100px"});
-    	$("#sample-textbox").css({"top":"210px", "left":"950px"});
-    	$("#tos-button").css({"top":"410px", "left":"1100px"});
+    	$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"150px", "left":"100px"});
+    	$("#sample-textbox").css({"top":"160px", "left":"950px"});
+    	$("#tos-button").css({"top":"510px", "left":"1200px"});
 	}
 	else{
 		$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"100px"});
