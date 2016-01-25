46baa9470eb6c9781230143f6b702a485f1d45ef
charles <charles.98006@gmail.com>
2016-01-25 00:52:35 -0800

    DONE for IE

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function skipClick(){
 function transitVid(){
 	$( ".jp-video" ).fadeOut( "slow", function(){
 	if (navigator.appVersion.indexOf("MSIE 10") !== -1){
-    	// $( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"150px", "left":"100px"});
+    	$( "#jp_container_1" ).css({"width":"800px", "height":"575px","background":"none", "margin":"10px", "top":"150px", "left":"100px"});
     	// $("#sample-textbox").css({"top":"160px", "left":"950px"});
     	// $("#tos-button").css({"top":"610px", "left":"1240px"});
 	}
